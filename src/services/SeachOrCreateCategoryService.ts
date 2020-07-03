a16c497b160dd71cdf151f4f707d94876c87bb13
Thiago Mônaco <tmonaco@epharma.com.br>
2020-07-03 12:06:26 -0300

    versao inicial

---
+++ src/services/SeachOrCreateCategoryService.ts
@@ -0,0 +1,30 @@
+﻿import { getRepository } from 'typeorm';
+import Category from '../models/Category';
+
+interface Request {
+  category: string;
+}
+
+class SeachOrCreateCategoryService {
+  public async execute({ category }: Request): Promise<Category> {
+    const categoryRepository = getRepository(Category);
+
+    const categorySearch = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categorySearch) {
+      const categoryCreate = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryCreate);
+
+      return categoryCreate;
+    }
+
+    return categorySearch;
+  }
+}
+
+export default SeachOrCreateCategoryService;
